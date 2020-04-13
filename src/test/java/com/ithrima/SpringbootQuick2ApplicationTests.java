358b5e343cf24a3dedf4d2e8dc99eac8b39cc032
1250498077 <asd912345qq.com@qq.com>
2020-04-14 07:43:08 +0800

    完成测试的编写工作

---
+++ src/test/java/com/ithrima/SpringbootQuick2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ithrima;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringbootQuick2ApplicationTests.class)
+class SpringbootQuick2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
